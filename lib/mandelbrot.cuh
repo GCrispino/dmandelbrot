d1781381f9c01f937c1086da7282cc6325f613b2
Gabriel Crispino <gabriel.crispino@hotmail.com>
2019-06-17 23:15:54 -0300

    WIP Implement MPI mandelbrot work distribution

--- lib/mandelbrot.cuh
+++ lib/mandelbrot.cuh
@@ -83,7 +83,81 @@ namespace mandelbrot{
                 break;
         }
 
-           
+    }
 
+    int * get_boundaries(int w, int h, int rank, int n_procs, int block_size, bool print = false){
+        int 
+            // n_blocks_per_line = w / block_size,
+            n_blocks_per_column = h / block_size;
+        int
+            block_line = (rank - 1) % n_blocks_per_column,
+            // block_column = rank % n_blocks_per_line,
+            // start_x = block_size * block_column,
+            start_x = 0,
+            end_x = w - 1,
+            // end_x = start_x + block_size < w - 1 ? start_x + block_size : w - 1,
+            start_y = block_size * block_line,
+            // start_y = 0,
+            // end_y = h - 1,
+            end_y = rank == n_procs ? h - 1 : start_y + block_size - 1,
+            // end_y = start_y + block_size - 1 < h - 1 ? start_y + block_size - 1 : h - 1,
+            *res = (int *) malloc(4 * sizeof(int));
+
+        if (print){
+            printf("\tn_procs: %d\n",n_procs);
+            printf("\trank %d, x: (%d, %d), y: (%d, %d)\n", rank, start_x, end_x, start_y, end_y);
+        }
+        res[0] = start_x;
+        res[1] = end_x;
+        res[2] = start_y;
+        res[3] = end_y;
+
+        return res;
+    }
+
+    unsigned get_block_size(unsigned w, unsigned h, unsigned n_procs){
+        unsigned block_size = 1;
+        while (block_size * 2 < (w / n_procs) && block_size * 2 < (h / n_procs)){
+            block_size *= 2;
+        }
+
+        return block_size;
+    }
+
+    void d_mandelbrot(
+        exec_mode ex, unsigned n_threads,
+        unsigned rank, unsigned n_procs,
+        complex<REAL_TYPE> c0, complex<REAL_TYPE> c1,
+        REAL_TYPE delta_x, REAL_TYPE delta_y,
+        unsigned w, unsigned h, unsigned m,
+        unsigned *table
+    ){
+
+        unsigned block_size = get_block_size(w, h, n_procs);
+        // printf("block_size %d\n",block_size);
+        int *bound = get_boundaries(w, h, rank, n_procs, block_size, true);
+        int start_y = bound[2], end_y = bound[3];
+        unsigned local_h = end_y - start_y;
+        unsigned local_table_size = w * local_h;
+        unsigned *local_table = new unsigned[local_table_size];
+        complex<REAL_TYPE> local_c0(c0.real(), c0.imag() + start_y * delta_y), local_c1(c1.real(), c1.imag() + start_y * delta_y);
+
+        mandelbrot::mandelbrot(
+            ex, n_threads,
+            local_c0, local_c1,
+            delta_x, delta_y,
+            w, local_h, m, local_table
+        );
+
+        printf("\tRank: %d will send %d!\n", rank, local_table_size);
+        MPI_Send(
+            local_table,
+            local_table_size,
+            MPI_UNSIGNED,
+            0,
+            0,
+            MPI_COMM_WORLD
+        );
+        printf("\tRank: %d sent data!\n", rank);
     }
 }
--- main.cu
+++ main.cu
@@ -12,7 +12,7 @@
 #else
 #   define  __CUDA__ 0
 #   define INCLUDE_FILE(x) QUOTEME(complex)
-#   define COMPLEX std 
+#   define COMPLEX std
 #endif
 
 #ifndef REAL_TYPE
@@ -27,10 +27,10 @@
 void print_table(unsigned w, unsigned h, unsigned ** table){
     for (unsigned i = 0;i < h; ++i){
         for (unsigned j = 0;j < w; ++j){
-            std::cout << table[i * h + j]  << ' '; 
+            std::cout << table[i * h + j]  << ' ';
         }
 
-        std::cout << std::endl; 
+        std::cout << std::endl;
     }
 }
 
@@ -97,15 +97,15 @@ struct params parse_args(int argc, char **argv){
     using COMPLEX::complex;
 
 
-    std::string 
-        usage("USAGE: mbrot <C0_REAL_TYPE> <C0_IMAG> <C1_REAL> <C1_IMAG> <W> <H> <CPU/GPU> <THREADS> <OUTPUT>");
+    std::string
+        usage("USAGE: dmbrot <C0_REAL_TYPE> <C0_IMAG> <C1_REAL> <C1_IMAG> <W> <H> <CPU/GPU> <THREADS> <OUTPUT>");
 
     if (argc != 10){
         std::cerr << usage << std::endl;
         exit(1);
     }
 
-    REAL_TYPE 
+    REAL_TYPE
         c0_real = atof(argv[1]), c0_imag = atof(argv[2]),
         c1_real = atof(argv[3]), c1_imag = atof(argv[4]);
 
@@ -121,8 +121,11 @@ struct params parse_args(int argc, char **argv){
     );
 }
 
+
 int main(int argc, char **argv){
     using COMPLEX::complex;
+
+    MPI_Init(&argc, &argv);
 
     params args = parse_args(argc, argv);
 
@@ -131,17 +134,57 @@ int main(int argc, char **argv){
 
     unsigned *table = new unsigned[w * h];
 
-	complex<REAL_TYPE> c0(args.c0),c1(args.c1);
+    complex<REAL_TYPE> c0(args.c0),c1(args.c1);
 
-	const REAL_TYPE delta_x = (c1.real() - c0.real()) / w;
-	const REAL_TYPE delta_y = (c1.imag() - c0.imag()) / h;
+    const REAL_TYPE delta_x = (c1.real() - c0.real()) / w;
+    const REAL_TYPE delta_y = (c1.imag() - c0.imag()) / h;
 
-    mandelbrot::mandelbrot(ex, args.n_threads, c0,c1,delta_x,delta_y,w,h,m,table);
-     
-    png::image< png::rgb_pixel > image = create_image(w,h,table);
-    image.write(args.output_path);
+    int world_size, world_rank;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    if (world_rank == 0){
+        unsigned block_size = mandelbrot::get_block_size(w, h, world_size - 1);
+
+        for (unsigned i = 1; i < world_size; ++i){
+            int *bound = mandelbrot::get_boundaries(w, h, i, world_size - 1, block_size);
+            int start_x = bound[0], end_x = bound[1], start_y = bound[2], end_y = bound[3];
+            unsigned local_h = end_y - start_y + 1;
+            unsigned local_table_size = w * local_h;
+            unsigned offset = block_size * w * (i - 1);
+
+            printf("\t%d - start_y: %d, end_y: %d, start_x: %d, end_x: %d, block_size: %d, table_size: %d, offset: %d\n", i, start_y, end_y, start_x, end_x, block_size, local_table_size, offset);
+            printf("\tRank: %d will receive %d!\n", i, local_table_size);
+            MPI_Recv(
+                table + offset, // TODO: Calc table offset according to rank
+                local_table_size,
+                MPI_UNSIGNED,
+                i,
+                0,
+                MPI_COMM_WORLD,
+                MPI_STATUS_IGNORE
+            );
+            printf("\tRank: %d received data!\n", i);
+        }
+            
+        png::image< png::rgb_pixel > image = create_image(w,h,table);
+        image.write(args.output_path);
+
+    }
+    else{
+        d_mandelbrot(
+            ex, args.n_threads,
+            world_rank, world_size - 1,
+            c0, c1,
+            delta_x, delta_y,
+            w, h, m, table
+        );
+    }
 
     delete[] table;
 
-	return 0;
+    MPI_Finalize();
+
+    return 0;
 }
